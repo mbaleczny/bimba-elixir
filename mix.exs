057b8409d00291018757a8a31c146ecf096fa2ed
Mariusz Bałęczny <mariuszbaleczny@gmail.com>
2022-01-14 18:12:41 +0100

    chore: add credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -3,23 +3,36 @@ defmodule Postgres.Umbrella.MixProject do
 
   def project do
     [
-      apps_path: "apps",
       aliases: aliases(),
+      apps_path: "apps",
       deps: deps(),
+      dialyzer: dialyzer(),
       start_permanent: Mix.env() == :prod,
       version: "0.1.0"
     ]
-  end
-
-  defp deps do
-    []
   end
 
   defp aliases do
     [
       # run `mix setup` in all child apps
       setup: ["cmd mix setup"],
+      credo: ["credo --strict"],
       test: ["cmd mix test"]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      flags: [:error_handling, :race_conditions, :underspecs, :unmatched_returns],
+      ignore_warnings: ".dialyzerignore",
+      plt_add_apps: [:mix]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
     ]
   end
 end
